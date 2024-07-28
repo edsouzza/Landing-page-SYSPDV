9ede6e0505f9236be194ceb139be067e5e769da0
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-07-28 12:58:25 -0300

    feat: update selection of menu add color in menu selected

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+document.querySelectorAll('.link').forEach(link => {
+    link.addEventListener('click', function(event) {
+        // Remove a classe 'ativo' de todos os links
+        document.querySelectorAll('.link').forEach(link => link.classList.remove('ativo'));
+        
+        // Adiciona a classe 'ativo' ao link clicado
+        event.currentTarget.classList.add('ativo');
+    });
+});
